41f5a38309b54ad5182664060441ad702956be67
broadstreetads <broadstreetads@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-29 03:39:40 +0000

    Broadstreet release 1.1.0, now with a business directory
    git-svn-id: https://plugins.svn.wordpress.org/broadstreet/trunk@631571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- broadstreet.php
+++ broadstreet.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Broadstreet
 Plugin URI: http://broadstreetads.com
-Description: Integrate your Broadstreet ad zones via a widget
-Version: 1.0.0
+Description: Integrate Broadstreet business directory and adserving power into your site
+Version: 1.1.0
 Author: Broadstreet
 Author URI: http://broadstreetads.com
 */
