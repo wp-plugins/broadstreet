6e89806548fa90be7612bbb7a982636ad89444df
broadstreetads <broadstreetads@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-31 19:20:41 +0000

    Fixed account linkage regression bug
    git-svn-id: https://plugins.svn.wordpress.org/broadstreet/tags/1.3.2@646469 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Broadstreet/Ajax.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This file contains a class which provides the AJAX callback functions required
+ *  for Broadstreet.
+ *
+ * @author Broadstreet Ads <labs@broadstreetads.com>
+ */
+
+/**
+ * A class containing functions for the AJAX functionality in Broadstreet. These
+ *  aren't executed directly by any Broadstreet code -- they are registered with
+ *  the Wordpress hooks in Broadstreet_Core::_registerHooks(), and called as needed
+ *  by the front-end and Wordpress. All of these methods output JSON.
+ */
+class Broadstreet_Ajax
+{
+    /**
+     * Save a boolean value of whether to index comments on the next rebuild
+     */
+    public static function saveSettings()
+    {
+        Broadstreet_Utility::setOption(Broadstreet_Core::KEY_API_KEY, $_POST['api_key']);
+        Broadstreet_Utility::setOption(Broadstreet_Core::KEY_NETWORK_ID, $_POST['network_id']);
+        Broadstreet_Utility::setOption(Broadstreet_Core::KEY_BIZ_ENABLED, $_POST['business_enabled'] === 'true');
+        
+        $api = new Broadstreet($_POST['api_key']);
+
+        try
+        {
+            $networks  = $api->getNetworks();
+            $key_valid = true;
+            
+            if($_POST['network_id'] == '-1')
+            {
+                Broadstreet_Utility::setOption(Broadstreet_Core::KEY_NETWORK_ID, $networks[0]->id);
+            }
+            
+            //Broadstreet_Utility::refreshZoneCache();
+        }
+        catch(Exception $ex)
+        {
+            $networks = array();
+            $key_valid = false;
+            
+            # Clear any options that aren't valid following the failed API key config
+            Broadstreet_Utility::setOption(Broadstreet_Core::KEY_BIZ_ENABLED, FALSE);
+        }
+        
+        die(json_encode(array('success' => true, 'key_valid' => $key_valid, 'networks' => $networks)));
+    }
+    
+    public static function createAdvertiser()
+    {
+        $api_key    = Broadstreet_Utility::getOption(Broadstreet_Core::KEY_API_KEY);
+        $network_id = Broadstreet_Utility::getOption(Broadstreet_Core::KEY_NETWORK_ID);
+        
+        $api        = new Broadstreet($api_key);
+        $advertiser = $api->createAdvertiser($network_id, stripslashes($_POST['name']));
+        
+        die(json_encode(array('success' => true, 'advertiser' => $advertiser)));
+    }
+    
+    public static function importFacebook()
+    {
+        try
+        {
+            $profile = Broadstreet_Utility::importBusiness($_POST['id'], $_POST['post_id']);
+            die(json_encode(array('success' => (bool)$profile, 'profile' => $profile)));
+        } 
+        catch(Broadstreet_ServerException $ex)
+        {
+            die(json_encode(array('success' => false, 'message' => ($ex->error ? $ex->error->message : 'Server error. This issue has been reported to the folks at Broadstreet.'))));
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Plugin Name ===
+Contributors: Broadstreet
+Tags: broadstreet,local,publishers,hyperlocal,independent,news,business,directory
+Requires at least: 3.0
+Tested up to: 3.4.2
+Stable tag: 1.3.2
+
+Integrate Broadstreet business directory and adserving power into your site.
+
+== Description ==
+
+For Broadstreet users. If you aren't a user, you can sign up after installing.
+
+Introducing first business directory of it's kind, this Broadstreet-powered
+plug-in is the first practical and portable business directory for Wordpress.
+
+This was built for local publishers.
+
+* Magic import: Automatically crawl the web to fill in business details and photos
+* Business updates: Allow businesses to post a message on their profile via text message, Facebook, or Twitter
+* Easily create a photo gallery for each business
+
+... And everything else you would expect of a business directory.
+
+Broadstreet was build by the developers of a local, independent news blog in
+Red Bank, NJ. We built it to help monetize our much-loved community site.
+If you're a local publisher struggling to monetize your site, Broadstreet is
+for you.
+
+PS: You can easily integrate your Broadstreet adserver zones with this
+plugin too.
+
+**How to:**
+
+1. Install the plugin
+2. Go to Settings->Broadstreet
+3. Enter your Access Token and confirm that it's valid (we'll check automatically)
+4. Go to Appearance->Widgets, and use the new 'Broadstreet Ad Zone' widget
+
+To learn more about Broadstreet, and how it can help you as a local publisher,
+send an email to [kenny@broadstreetads.com](kenny@broadstreetads.com).
+
+== Fix Log ==
+
+* 1.2.3: Fixed image upload bug affecting minority of WP installations
