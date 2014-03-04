0451da4966ef29360b860d8146a11536ab18c610
broadstreetads <broadstreetads@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-04 20:03:18 +0000

    Fix conflicting javascript creating missing zone and network info
    git-svn-id: https://plugins.svn.wordpress.org/broadstreet/trunk@869475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Broadstreet/Views/listings/index.php
+++ Broadstreet/Views/listings/index.php
@@ -8,23 +8,30 @@
     }
     
     .broadstreet-index h4 {
+        margin-bottom: 4px;
         margin-bottom: 8px;
     }
     
 </style>
 
+<div class="broadstreet-index-right" style="width: 40%; box-sizing: border-box; padding-right: 5px; float: right">
+    <?php dynamic_sidebar('businesses-right-sidebar') ?>
+</div>
+
+
 <?php $i = 0; foreach($cats_to_posts as $cat_id => $data): ?>
 
-<div class="broadstreet-index" style="width: 50%; box-sizing: border-box; padding-right: 5px; float: <?php echo $i % 2 == 0 ? 'left' : 'right' ?>;">
+<div class="broadstreet-index" style="width: 60%; box-sizing: border-box; padding-right: 5px; float: left">
 
     <h4><?php echo htmlentities($data['name']) ?></h4>
     
     <ul>
         <?php foreach($data['posts'] as $post): ?>
-        <li><a href="<?php echo get_permalink($post->ID) ?>"><?php echo htmlentities($post->post_title) ?></a></li>
+        <li><a href="<?php echo get_permalink($post->ID) ?>"><?php echo $post->post_title ?></a></li>
         <?php endforeach; ?>
     </ul>
     
 </div>
 
 <?php $i++; endforeach; ?>
+
